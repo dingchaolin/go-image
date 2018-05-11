4ada7ac88952f08aa35ff50311502ea4f9916fcc
dingchaolin <18810042351@163.com>
2018-05-11 18:15:13 +0800

    feat: init

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # go-image
+
+## mac环境搭建
+
+### libvips 安装
+- 下载 https://github.com/jcupitt/libvips/releases/download/v8.6.3/vips-8.6.3.tar.gz
+- 解压
+- ./configure
+- make
+- sudo make install
+
+## centos 安装
+- 下载 wget https://github.com/jcupitt/libvips/releases/download/v8.6.3/vips-8.6.3.tar.gz
+- tar -zxf vips-8.6.3.tar.gz
+- mv vips-8.6.3.tar vips
+- ./configure
+- make install
+
+#### centos 安装过程出现的问题 可能需要安装一下包
+- yum install libpng-devel
+- yum install glib2-devel
+- yum -y install expat-devel
+
+## centos 安装go
+- yum install go
+
+## Package vips was not found in the pkg-config search path
+-  export PKG_CONFIG_PATH=/usr/lib64/pkgconfig:/usr/share/pkgconfig:/usr/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig
+
---
+++ src/image_operate/iamge.go
@@ -0,0 +1,152 @@
+/**
+ * Created by chaolinding on 2018/5/11.
+ */
+
+package image_operate
+
+import (
+	bimg "gopkg.in/h2non/bimg.v1"
+	"errors"
+	"math"
+)
+
+/*
+裁剪
+ */
+func CropWithPath(filePath, savePath string, width, height, quality int) error {
+
+	if len(filePath) == 0 || len(savePath) == 0 {
+		return errors.New("文件路径参数有误")
+	}
+	if width <= 10 || height <= 10 {
+		return errors.New("裁剪宽高参数有误")
+	}
+	if quality < 0 {
+		return errors.New("quality参数有误")
+	}
+
+	buffer, err := bimg.Read(filePath)
+	if err != nil {
+		return err
+	}
+
+	metaData, err := bimg.Metadata(buffer)
+	if err != nil {
+		return err
+	}
+
+	var options bimg.Options
+	if metaData.Alpha {
+		options = bimg.Options{
+			Height:     height,
+			Width:      width,
+			Background: bimg.Color{255, 255, 255},
+			Crop:       true,
+			Quality:    quality,
+			Gravity:    bimg.GravityCentre,
+		}
+	} else {
+		options = bimg.Options{
+			Height:  height,
+			Width:   width,
+			Crop:    true,
+			Quality: quality,
+			Gravity: bimg.GravityCentre,
+		}
+	}
+
+	newImage, err := bimg.NewImage(buffer).Process(options)
+	if err != nil {
+		return err
+	}
+
+	return bimg.Write(savePath, newImage)
+
+}
+
+/*
+变换大小
+ */
+func ResizeWithPath(filePath, savePath string, width, height, quality int) error {
+
+	if len(filePath) == 0 || len(savePath) == 0 {
+		return errors.New("文件路径参数有误")
+	}
+	if width < 0 || height < 0 || (width == 0 && height <= 10) || (height == 0 && width <= 10) {
+		return errors.New("裁剪宽高参数有误")
+	}
+	if quality < 0 {
+		return errors.New("quality参数有误")
+	}
+
+	buffer, err := bimg.Read(filePath)
+	if err != nil {
+		return err
+	}
+
+	metaData, err := bimg.Metadata(buffer)
+	if err != nil {
+		return err
+	}
+
+	if (width > 10 && height == 0) || (width == 0 && height > 10) {
+		if metaData.Size.Width <= 10 || metaData.Size.Height <= 10 {
+			return errors.New("图片meta宽高有误")
+		}
+		if height == 0 {
+			height = int(math.Floor(float64(width) * float64(metaData.Size.Height) / float64(metaData.Size.Width)))
+		} else if width == 0 {
+			width = int(math.Floor(float64(metaData.Size.Width) / float64(width) * float64(metaData.Size.Height)))
+		}
+	}
+
+	var options bimg.Options
+	if metaData.Alpha {
+		options = bimg.Options{
+			Height:     height,
+			Width:      width,
+			Background: bimg.Color{255, 255, 255},
+			Quality:    quality,
+		}
+	} else {
+		options = bimg.Options{
+			Height:  height,
+			Width:   width,
+			Quality: quality,
+		}
+	}
+
+	newImage, err := bimg.NewImage(buffer).Process(options)
+	if err != nil {
+		return err
+	}
+
+	return bimg.Write(savePath, newImage)
+}
+
+func FixImageWithPath(filePath, savePath string, width, height, quality int) error {
+	if width <= 10 || height <= 10 {
+		return errors.New("宽高参数有误")
+	}
+
+	buffer, err := bimg.Read(filePath)
+	if err != nil {
+		return err
+	}
+
+	options := bimg.Options{
+		Height:     height,
+		Width:      width,
+		Background: bimg.Color{255, 255, 255},
+		Quality:    quality,
+		Embed:      true,
+	}
+
+	newImage, err := bimg.NewImage(buffer).Process(options)
+	if err != nil {
+		return err
+	}
+
+	return bimg.Write(savePath, newImage)
+
+}
